f69b6035beb6af575934d25ea93de6929c366ef9
MUEHEHEHEH <143472197+MUEHEHEHEH@users.noreply.github.com>
2024-08-09 19:46:20 +0700

    update page

--- src/components/Award.jsx
+++ src/components/Award.jsx
@@ -1,5 +1,53 @@
-export default function Award() {
-    return(
-        <div className="font-bold flex justify-center">AWARD</div>
-    )
-}
+import React from "react";
+import paperBlueLight from "../assets/cover/paperBlueLight.svg";
+
+const Award = () => {
+  return (
+    <div className="bg-paper-blue-light w-full md:h-[60rem] h-auto my-36 bg-center lg:bg-cover bg-no-repeat self-stretch">
+      <div className="flex flex-col items-start justify-start px-8 pt-16 md:px-24 md:pt-24">
+        <h1 className="font-MadeMirage text-4xl lg:text-6xl leading-[120%] text-primary-50 font-bold self-stretch">
+          Award of Participations
+        </h1>
+        <h6 className="font-MadeMirage text-base md:text-2xl lg:text-4xl font-medium leading-[120%] text-primary-50 self-stretch mb-6">
+          Join the competition and win some great prizes!
+        </h6>
+
+        <div className="flex flex-col lg:flex-row items-center justify-center md:gap-x-4 px-auto mb-4 gap-4 md:gap-8 lg:gap-4">
+          <div className="border-4 border-secondary-50 rounded-[2rem] w-[20rem] md:w-[30rem] h-[16rem] lg:w-[40rem] lg:h-[34.5rem] p-3 flex flex-col items-center gap-2 flex-shrink-0 self-stretch">
+            <div
+              className="border bg-money-mobile md:bg-cover md:bg-money bg-no-repeat w-full h-full flex flex-col justify-center items-center gap-2 flex-shrink-0 rounded-[2rem]"
+              style={{
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+              }}
+            ></div>
+          </div>
+
+          <div className="flex flex-col items-center justify-between w-full gap-4 lg:gap-8">
+            <div className="border-4 border-secondary-50 rounded-[2rem] w-[20rem] md:w-[30rem] h-[11rem] lg:w-[40rem] lg:h-[16rem] p-3 flex flex-col items-center gap-2 flex-shrink-0 self-stretch">
+              <div
+                className="border bg-certificate-mobile md:bg-certificate bg-cover bg-no-repeat bg-center w-full h-full flex flex-col justify-center items-center gap-2 flex-shrink-0 rounded-[2rem]"
+                style={{
+                  backgroundSize: "cover",
+                  backgroundPosition: "center",
+                }}
+              ></div>
+            </div>
+
+            <div className="border-4 border-secondary-50 rounded-[2rem] w-[20rem] md:w-[30rem] h-[11rem] lg:w-[40rem] lg:h-[16rem] p-3  flex flex-col items-center gap-2 flex-shrink-0 self-stretch">
+              <div
+                className="border bg-medal-mobile md:bg-medal bg-cover bg-no-repeat bg-center w-full h-full flex flex-col justify-center items-center gap-2 flex-shrink-0 rounded-[2rem]"
+                style={{
+                  backgroundSize: "cover",
+                  backgroundPosition: "center",
+                }}
+              ></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Award;
--- src/components/WhatAbstract.jsx
+++ src/components/WhatAbstract.jsx
@@ -1,5 +1,50 @@
+import React from "react";
+import Rectangle from "../assets/image/Rectangle.svg";
+import paperBlue from "../assets/cover/paperBlue.png";
+import intersect from "../assets/image/intersect.svg";
+
 export default function WhatAbstract() {
-    return(
-        <div className="font-bold flex justify-center">WHAT EXTENDED ABSTRACT IS?</div>
-    )
+  return (
+    <div
+      className="relative flex items-start mt-36"
+      style={{
+        backgroundImage: `url(${paperBlue})`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <div className="relative flex flex-col lg:flex-row items-center justify-evenly lg:justify-between w-full h-full px-4 lg:px-20">
+        <div className="flex items-center z-20">
+          <img
+            src={Rectangle}
+            className="absolute hidden lg:block w-[20rem] h-[35rem] mt-20"
+            alt="Rectangle"
+          />
+        </div>
+
+        <div className="flex flex-col justify-center w-full lg:w-[48rem] h-auto lg:h-[12rem] text-primary-50 container z-30">
+          <h1 className="leading-[3rem] lg:leading-[4rem] font-MadeMirage text-4xl lg:text-6xl font-bold mb-4 lg:mb-8 flex justify-center text-center lg:justify-start flex-shrink-0 items-stretch">
+            What Extended Abstract Is?
+          </h1>
+          <p className="font-Metropolis text-lg lg:text-xl font-medium leading-normal mb-12 lg:mb-16 items-stretch text-center lg:text-justify">
+            Extended Abstract is a longer summary of a paper which is more
+            comprehensive than a typical abstract. The purpose is to provide a
+            more detailed overview of the paper or research, allowing readers to
+            understand the essence and contribution of the study without having
+            to read the entire document.
+          </p>
+          <div className="flex justify-center lg:justify-start items-center gap-12 lg:gap-5">
+            <button className="bg-primary-50 rounded-[4rem] text-secondary-90 text-center font-Metropolis leading-5 text-sm lg:text-md font-semibold py-2 lg:py-3 px-8 lg:px-16 flex justify-center items-center hover:bg-secondary-70 hover:text-primary-50">
+              Guidebook
+            </button>
+            <button className="bg-primary-50 rounded-[4rem] text-secondary-90 text-center font-Metropolis leading-5 text-sm lg:text-md font-semibold py-2 lg:py-3 px-8 lg:px-16 flex justify-center items-center hover:bg-secondary-70 hover:text-primary-50">
+              Register Now
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
